857b09e284282f7a09c14159869ceb88be23ca26
Justin Vrieling <justin.vrieling@hotmail.com>
2019-10-11 19:19:56 -0400

    Begin implementing PlayerController & manual gravity

---
+++ JuminoKart/Assets/Scripts/Gravity.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    [SerializeField]
+    private float weight = 1;
+
+    [SerializeField]
+    float terminalVelocity = 10;
+
+    [SerializeField]
+    private float gravityPull = 9.8f;
+
+    [SerializeField]
+    Vector2 velocity;
+
+    private Collider2D colliderComponent;
+
+    private void Awake()
+    {
+        colliderComponent = GetComponent<Collider2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 velocityModification = new Vector2
+        {
+            y = -(weight * gravityPull * Time.fixedDeltaTime)
+        };
+        velocity += velocityModification;
+
+        transform.Translate(velocity);
+    }
+
+    void checkCollisions()
+    {
+        Collider2D[] collisions = new Collider2D[0];        
+    }
+}
---
+++ JuminoKart/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpHeight = 3;
+    [SerializeField]
+    [Tooltip("Movement speed in units per second")]
+    private float movementSpeed = 1;
+
+    private Collider2D colliderComponent;
+
+    private void Awake()
+    {
+        colliderComponent = GetComponent<Collider2D>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
